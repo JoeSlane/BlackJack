201f509570863306c8ce02de7b24343093aa64fe
Joe Slane <joe.slane08@gmail.com>
2023-11-22 22:07:23 +0000

    second commit, mostly done now

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-let firstCard = 5
-let secondCard = 8 
-let sum = firstCard + secondCard
+let firstCard = Math.floor(Math.random() * (1, 11)) 
+let secondCard = Math.floor(Math.random() * (1, 11)) 
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let card = "";
+let min = 1
+let max = 11
 
 
-if (sum <= 20) {
-    console.log("do you want to draw a new card?")
+function startGame() {
+  renderGame();
 }
-else if (sum === 21) {
-    console.log("You got Blackjack")
+
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + "," + secondCard + "," + card;
+  if (sum <= 20) {
+    message = "do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You got Blackjack";
+    hasBlackJack = true;
+  } else {
+    message = "You failed";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
-else {
-    console.log("You failed")
+function newCard() {
+  console.log("new card");
+  card = Math.floor(Math.random() * (min, max)) 
+  sum += card;
+  renderGame();
 }
